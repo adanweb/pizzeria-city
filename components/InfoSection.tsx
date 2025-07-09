bf70e39c7a22bffd05393c714eaff3ca9476895f
adanweb <adan.skolski@gmail.com>
2025-07-09 22:39:00 +0200

    WH Update

--- components/InfoSection.tsx
+++ components/InfoSection.tsx
@@ -37,7 +37,7 @@ const InfoSection: React.FC<InfoSectionProps> = ({ t }) => {
           <div className="space-y-6">
             <InfoItem icon={<MapPinIcon />} title={t.address} value={t.addressValue} />
             <InfoItem icon={<PhoneIcon />} title={t.phone} value="+387 37 223-565" />
-            <InfoItem icon={<ClockIcon />} title={t.workingHours} value={t.workingHoursValue} />
+            <InfoItem icon={<ClockIcon />} title={t.workingHours} value={<span className="whitespace-pre-line">{t.workingHoursValue}</span>} />
             <div>
               <h3 className="text-lg font-semibold text-amber-400 mb-2">{t.socialMedia}</h3>
               <div className="flex space-x-4">
