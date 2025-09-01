c3ed2e566df3aab0fdb44d2fd4aa8e62040efc95
adanweb <adan.skolski@gmail.com>
2025-09-01 12:06:17 +0200

    Test ramstek

--- constants/data.ts
+++ constants/data.ts
@@ -8,7 +8,7 @@ export const menuData: MenuCategory[] = [
   id: 'special',
   title: t('Specijalna ponuda', 'Special Offer', 'Spezialangebot', 'العرض الخاص'),
   items: [
-    { name: t('Ramstek', 'Rump Steak', 'Rumpsteak', 'رامب ستيك'), price: '17,00 KM' },
+    { name: t('Ramstek', 'Rump Steak', 'Rumpsteak', 'رامب ستيك'), price: '18,00 KM' },
     { name: t('Ramstek sa gorgonzolom', 'Rump Steak with Gorgonzola', 'Rumpsteak mit Gorgonzola', 'رامب ستيك مع الجورجونزولا'), price: '19,00 KM' },
     { name: t('Biftek', 'Beefsteak', 'Rinderfilet', 'بيفتك'), price: '31,00 KM' },
     { name: t('Biftek sa gorgonzolom', 'Beefsteak with Gorgonzola', 'Rinderfilet mit Gorgonzola', 'بيفتك مع الجورجونزولا'), price: '33,00 KM' },
